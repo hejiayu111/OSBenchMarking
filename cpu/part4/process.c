6dc78d1244826f872a7ce1a500825a9ab87b301c
ShangGaogit <shgao@eng.ucsd.edu>
2020-03-10 18:28:50 -0700

    process creation

--- cpu/part4/process.c
+++ cpu/part4/process.c
@@ -4,42 +4,31 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
+#define LOOP 100
+#define rdtsc_time 120
+
 int main(int argc, char **argv) {
-    
-    if(argc < 2) {
-        printf("usage : ./program 1000");
-        exit(0);
-    }
-    
-    unsigned long long overhead = 133221;
-    
-    int loops = atoi(argv[1]);
     unsigned long long total = 0;
     unsigned long long start, end;
-    
     int i = 0;
     
-    for (; i < loops; i++) {
-        
+    for (; i < LOOP; i++) {
         start = rdtsc();
         pid_t pid = fork();
-        
-        if (pid == -1) {
-            printf("fork failed!\n");
-            exit(-1);
-        }
-        
+
         if (pid == 0) {
-            exit(-1);
-        } else {
+            return 0;
+        } 
+        
+        else {
             wait(NULL);
             end = rdtsc();
-            unsigned long long diff = end - start;
-            total += (diff - 2*overhead);
+            total += (end - start - rdtsc_time);
         }
     }
 
-    printf("fork task creation : %llu cycles; Total : %llu \n", total/loops, total);
+    printf("process creation averagely takes %f cycles\n", 1.0 * total/LOOP);
 
     return 0;
 }

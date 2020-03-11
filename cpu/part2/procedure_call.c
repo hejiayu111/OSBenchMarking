b2862ccc21ed00e18dda0864a6edfc1e64f114c6
hejiayu111 <jihe@eng.ucsd.edu>
2020-03-11 08:37:27 +0800

    refactor

--- cpu/part2/procedure_call.c
+++ cpu/part2/procedure_call.c
@@ -3,147 +3,141 @@
 #include "stdio.h"
 
 #pragma OPTIMIZE OFF
+#define LOOP 1000000
 
-void __attribute__ ((noinline)) foo0() {}
-void __attribute__ ((noinline)) foo1(int arg1) {}
-void __attribute__ ((noinline)) foo2(int arg1, int arg2) {}
-void __attribute__ ((noinline)) foo3(int arg1, int arg2, int arg3) {}
-void __attribute__ ((noinline)) foo4(int arg1, int arg2, int arg3, int arg4) {}
-void __attribute__ ((noinline)) foo5(int arg1, int arg2, int arg3, int arg4, int arg5) {}
-void __attribute__ ((noinline)) foo6(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6) {}
-void __attribute__ ((noinline)) foo7(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7) {}
+void function0() {}
+void function1(int arg1) {}
+void function2(int arg1, int arg2) {}
+void function3(int arg1, int arg2, int arg3) {}
+void function4(int arg1, int arg2, int arg3, int arg4) {}
+void function5(int arg1, int arg2, int arg3, int arg4, int arg5) {}
+void function6(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6) {}
+void function7(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7) {}
 
-int loops;
 
-void fun0() {
+void meaure0() {
     int i = 0;
-    unsigned long long total0 = 0;
+    unsigned long long total = 0;
     unsigned long long start, end;
     start = rdtsc();
-    for(;i < loops;++i) {
-        foo0();
+    for(;i < LOOP;++i) {
+        function0();
     }
     end = rdtsc();
-    total0 = end - start;
-    printf("0 argument procedure call overhead is : %f cycles; Total : %llu \n", total0*1.0/loops, total0);
+    total = end - start;
+    printf("overhead of 0 argument function is: %f cycles; Total : %llu \n", total*1.0/LOOP, total);
 }
 
-void fun1() {
+void measure1() {
     int i = 0;
-    unsigned long long total0 = 0;
+    unsigned long long total = 0;
     unsigned long long start, end;
     volatile int a;
     start = rdtsc();
-    for(;i < loops;++i) {
-        foo1(a);
+    for(;i < LOOP;++i) {
+        function1(a);
     }
     end = rdtsc();
-    total0 = end - start;
-    printf("1 argument procedure call overhead is : %f cycles; Total : %llu \n", total0*1.0/loops, total0);
+    total = end - start;
+    printf("overhead of 1 argument function is: %f cycles; Total : %llu \n", total*1.0/LOOP, total);
 }
 
-void fun2() {
+void measure2() {
     int i = 0;
-    unsigned long long total0 = 0;
+    unsigned long long total = 0;
     unsigned long long start, end;
     volatile int a,b;
     start = rdtsc();
-    for(;i < loops;++i) {
-        foo2(a,b);
+    for(;i < LOOP;++i) {
+        function2(a,b);
     }
     end = rdtsc();
-    total0 = end - start;
-    printf("2 argument procedure call overhead is : %f cycles; Total : %llu \n", total0*1.0/loops, total0);
+    total = end - start;
+    printf("overhead of 2 arguments function is: %f cycles; Total : %llu \n", total*1.0/LOOP, total);
 }
 
-void fun3() {
+void measure3() {
     int i = 0;
-    unsigned long long total0 = 0;
+    unsigned long long total = 0;
     unsigned long long start, end;
     volatile int a,b,c;
-    start = rdtsc();
-    for(;i < loops;++i) {
-        foo3(a,b,c);
+        start = rdtsc();
+    for(;i < LOOP;++i) {
+        function3(a,b,c);
     }
     end = rdtsc();
-    total0 = end - start;
-    printf("3 argument procedure call overhead is : %f cycles; Total : %llu \n", total0*1.0/loops, total0);
+    total = end - start;
+    printf("overhead of 3 arguments function is: %f cycles; Total : %llu \n", total*1.0/LOOP, total);
 }
 
-void fun4() {
+void measure4() {
     int i = 0;
-    unsigned long long total0 = 0;
+    unsigned long long total = 0;
     unsigned long long start, end;
     volatile int a,b,c,d;
     start = rdtsc();
-    for(;i < loops;++i) {
-        foo4(a,b,c,d);
+    for(;i < LOOP;++i) {
+        function4(a,b,c,d);
     }
     end = rdtsc();
-    total0 = end - start;
-    printf("4 argument procedure call overhead is : %f cycles; Total : %llu \n", total0*1.0/loops, total0);
+    total = end - start;
+    printf("overhead of 4 arguments function is: %f cycles; Total : %llu \n", total*1.0/LOOP, total);
 }
 
-void fun5() {
+void measure5() {
     int i = 0;
-    unsigned long long total0 = 0;
+    unsigned long long total = 0;
     unsigned long long start, end;
     volatile int a,b,c,d,e;
-    start = rdtsc();
-    for(;i < loops;++i) {
-        foo5(a,b,c,d,e);
+        start = rdtsc();
+    for(;i < LOOP;++i) {
+        function5(a,b,c,d,e);
     }
     end = rdtsc();
-    total0 = end - start;
-    printf("5 argument procedure call overhead is : %f cycles; Total : %llu \n", total0*1.0/loops, total0);
+    total = end - start;
+    printf("overhead of 5 argument function is: %f cycles; Total : %llu \n", total*1.0/LOOP, total);
 }
 
-void fun6() {
+void measure6() {
     int i = 0;
-    unsigned long long total0 = 0;
+    unsigned long long total = 0;
     unsigned long long start, end;
     volatile int a,b,c,d,e,f;
     start = rdtsc();
-    for(;i < loops;++i) {
-        foo6(a,b,c,d,e,f);
+    for(;i < LOOP;++i) {
+        function6(a,b,c,d,e,f);
     }
     end = rdtsc();
-    total0 = end - start;
-    printf("6 argument procedure call overhead is : %f cycles; Total : %llu \n", total0*1.0/loops, total0);
+    total = end - start;
+    printf("overhead of 6 argument function is: %f cycles; Total : %llu \n", total*1.0/LOOP, total);
 }
 
 
-void fun7() {
+void measure7() {
     int i = 0;
-    unsigned long long total0 = 0;
+    unsigned long long total = 0;
     unsigned long long start, end;
     volatile int a,b,c,d,e,f,g;
     start = rdtsc();
-    for(;i < loops;++i) {
-        foo7(a,b,c,d,e,f,g);
+    for(;i < LOOP;++i) {
+        function7(a,b,c,d,e,f,g);
     }
     end = rdtsc();
-    total0 = end - start;
-    printf("7 argument procedure call overhead is : %f cycles; Total : %llu \n", total0*1.0/loops, total0);
+    total = end - start;
+    printf("overhead of 7 argument function is: %f cycles; Total : %llu \n", total*1.0/LOOP, total);
 }
-
-int main(int argc, char **argv) {
     
-    if(argc < 2) {
-        printf("usage : ./program 1000000");
-        exit(0);
-    }
+int main() {
     
-    loops = atoi(argv[1]);
-
-    fun0();
-    fun1();
-    fun2();
-    fun3();
-    fun4();
-    fun5();
-    fun6();
-    fun7();
+    
+    meaure0();
+    measure1();
+    measure2();
+    measure3();
+    measure4();
+    measure5();
+    measure6();
+    measure7();
 
     return 0;
 }
---
+++ cpu/part2/procedure_call.sh
@@ -0,0 +1,2 @@
+gcc -O0 -I../../ procedure_call.c -o procedure_call.out
+./procedure_call.out
